afc7093c9b893e6f97f1e875d56323298d1fa669
Joshua Oguma <joshua.oguma@outlook.com>
2023-05-23 00:06:23 +0300

    fixed the pipeline

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('/app/pinglink-ui'))
+app.use(express.static('public'))
 
 app.listen(3000, () => {
     console.log('Web server running at port 3000');
