da1259317eaa11dc406ff9b21e140c81531bb125
Joshua Oguma <joshua.oguma@outlook.com>
2023-08-29 00:02:40 +0300

    added some changes

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('../pinglink/'))
-app.use(express.static('/app/pinglink-ui'))
+app.use(express.static('/app/ping-ui'))
 
 app.listen(3000, () => {
     console.log('Web server running at port 3000');
